1587a9fbab743a9cb25392c45d32db9b7a5fb7eb
mohamad-samir <mohamad.samir2011@gmail.com>
2023-09-03 20:14:26 +0300

    u

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -19,33 +19,33 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  * Return: lowest common ancestor
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-				     const binary_tree_t *second)
+									 const binary_tree_t *second)
 {
 	size_t height_f = 0, height_s = 0;
 	const binary_tree_t *aux = NULL;
 
-/* Calculate the depth of the first and second nodes */
+	/* Calculate the depth of the first and second nodes */
 	height_f = binary_tree_depth(first);
 	height_s = binary_tree_depth(second);
 
-/* Check if both nodes exist in the binary tree */
+	/* Check if both nodes exist in the binary tree */
 	if (height_f && height_s)
 	{
-/* If the height of the first node is greater, swap first and second */
+		/* If the height of the first node is greater, swap first and second */
 		if (height_f > height_s)
 		{
 			aux = first;
 			first = second;
 			second = aux;
 		}
-/* Iterate upwards from the first node to find the lowest common ancestor */
+		/* Iterate upwards from the first node to find the lowest common ancestor */
 		while (first)
 		{
 			aux = second;
-/* Iterate upwards from the second node */
+			/* Iterate upwards from the second node */
 			while (aux)
 			{
-/* If a common ancestor is found, return it as the LCA */
+				/* If a common ancestor is found, return it as the LCA */
 				if (first == aux)
 					return ((binary_tree_t *)first);
 				aux = aux->parent;
@@ -55,9 +55,9 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	}
 	else
 	{
-    /* If either of the nodes doesn't exist, return NULL (no LCA) */
+		/* If either of the nodes doesn't exist, return NULL (no LCA) */
 		return (NULL);
 	}
-    /* If no common ancestor is found, return NULL (no LCA) */
+	/* If no common ancestor is found, return NULL (no LCA) */
 	return (NULL);
 }
--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,27 +1,26 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_height - measures the height of a binary tree.
- *@tree: pointer to the root node of the tree to measure the height.
- * Return: if tree is NULL, your function must return 0.
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		int left = 0, right = 0;
+	size_t left_height, right_height;
 
-		if (tree->right)
-			right = 1 + binary_tree_height(tree->right);
-		if (tree->left)
-			left = 1 + binary_tree_height(tree->left);
-		if (left > right)
-			return (left);
-		else
-			return (right);
-	}
-	else
+	if (tree == NULL)
 		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
+
 /**
  * print_at_level - print node, especific level
  * @tree: pointer to the root node of the tree to traverse
@@ -40,31 +39,21 @@ void print_at_level(const binary_tree_t *tree, void (*func)(int), int level)
 			print_at_level(tree->right, func, level - 1);
 		}
 	}
-
 }
 
 /**
- * binary_tree_levelorder - goes through a binary tree in level-order traversal
+ * binary_tree_levelorder - goes through a binary tree using
+ * level-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node.
+ * @func: pointer to a function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-/* Variables to store the tree height and current level during traversal */
-	size_t h = 0, i = 1;
+	int height, i;
 
-/* Check if the tree and function pointer are valid */
-	if (tree && func)
-	{
-/* Determine the height of the binary tree */
-		h = binary_tree_height(tree);
-/* Traverse the tree in level-order from level 1 to the tree height + 1 */
-		while (i <= h + 1)
-		{
-/* Call the function for all nodes at the current level */
-			print_at_level(tree, func, i);
-			i++;
-		}
-	}
-
+	if (tree == NULL || func == NULL)
+		return;
+	height = binary_tree_height(tree) + 1;
+	for (i = 1; i <= height; i++)
+		print_at_level(tree, func, i);
 }
